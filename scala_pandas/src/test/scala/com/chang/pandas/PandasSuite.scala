994d240ddd2736cf73033fc3ff0f690c81979dcf
Chang chen <baibaichen@gmail.com>
2017-04-11 16:40:40 +0800

    1 pom   a) change line separator from windows(CRLF) to unix(LF)   b) update dependency form spark-mlib to spark-sql 2 gitignore   a) ignore target   b) remove src/test/ 3 add PandasSuite

---
+++ scala_pandas/src/test/scala/com/chang/pandas/PandasSuite.scala
@@ -0,0 +1,13 @@
+package com.chang.pandas
+
+import org.apache.spark.SparkFunSuite
+
+/**
+  * Created by ChangDev on 4/11/2017.
+  */
+class PandasSuite extends SparkFunSuite{
+
+  test("first test") {
+    assert(1 == 1)
+  }
+}
