eed3d71996fb05af3da8bfeda18a697b9ccb8e5d
Chang chen <baibaichen@gmail.com>
2017-04-13 15:51:11 +0800

    add dfZipWithIndex

--- scala_pandas/src/main/scala/com/chang/pandas/Pandas.scala
+++ scala_pandas/src/main/scala/com/chang/pandas/Pandas.scala
@@ -1,9 +1,18 @@
 package com.chang.pandas
 
-import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.types.{LongType, StructField, StructType}
+import org.apache.spark.sql.{DataFrame, Row}
 
 
 object Pandas {
+
+  def dfZipWithIndex(df: DataFrame, colName: String = "row_num") : DataFrame = {
+    df.sqlContext.createDataFrame(
+      df.rdd.zipWithIndex.map(r => Row.fromSeq(Seq(r._2) ++ r._1.toSeq) ),
+      StructType(Array(StructField(colName, LongType, false)) ++ df.schema.fields )
+    )
+  }
+
   def concat(left: DataFrame, right: DataFrame): DataFrame = {
 
     null
--- scala_pandas/src/test/scala/com/chang/pandas/PandasSuite.scala
+++ scala_pandas/src/test/scala/com/chang/pandas/PandasSuite.scala
@@ -1,13 +1,18 @@
 package com.chang.pandas
 
-import org.apache.spark.SparkFunSuite
+
+import org.apache.spark.sql.QueryTest
+import org.apache.spark.sql.test.SharedSQLContext
 
 /**
   * Created by ChangDev on 4/11/2017.
   */
-class PandasSuite extends SparkFunSuite{
+class PandasSuite extends QueryTest with SharedSQLContext{
 
-  test("first test") {
-    assert(1 == 1)
+  test("dfZipWithIndex") {
+    val zipped = Pandas.dfZipWithIndex(testData)
+    zipped.collect().foreach { x =>
+      assert(x.getAs[Long](0) + 1 == x(1))
+    }
   }
 }
